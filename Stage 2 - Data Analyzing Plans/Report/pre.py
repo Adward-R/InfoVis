280c8b1a185ef52c01d4670ce35a39d1c9166d7a
Adward-R <517908768@qq.com>
2014-12-29 17:15:51 +0800

    Minor bug fixes

--- Stage 2 - Data Analyzing Plans/Report/pre.py
+++ Stage 2 - Data Analyzing Plans/Report/pre.py
@@ -44,9 +44,52 @@ def relevantArticleNum():
                 else:
                     cnt+=1
         fileReader.close()
-    
+
+    time = []
     for item in num:
-        fileWriter.write(item+'\n')
+        if (len(item.split('/'))==3):
+            if (int(item.split('/')[0])>=2014):
+                time.append(item.split('/'))
+        else:
+            tmp = item.split(' ')
+            if (len(tmp)>=4):
+                tmp = tmp[0:3]
+            if (len(tmp)<=2):
+                continue
+            tmp.reverse();
+            if ("January"==tmp[1]):
+                tmp[1] = "01"
+            elif ("February"==tmp[1]):
+                tmp[1] = "02"
+            elif ("March"==tmp[1]):
+                tmp[1] = "03"
+            elif ("April"==tmp[1]):
+                tmp[1] = "04"
+            elif ("May"==tmp[1]):
+                tmp[1] = "05"
+            elif ("June"==tmp[1]):
+                tmp[1] = "06"
+            elif ("July"==tmp[1]):
+                tmp[1] = "07"
+            elif ("August"==tmp[1]):
+                tmp[1] = "08"
+            elif ("September"==tmp[1]):
+                tmp[1] = "09"
+            elif ("October"==tmp[1]):
+                tmp[1] = "10"
+            elif ("November"==tmp[1]):
+                tmp[1] = "11"
+            elif ("December"==tmp[1]):
+                tmp[1] = "12"
+            else:
+                tmp[1] = "00"
+            if (tmp[0]>=2014):
+                time.append(tmp)
+            
+    row = 0
+    for item in time:
+        fileWriter.write(str(row)+': '+item[0]+' '+item[1]+' '+item[-1]+'\n')
+        row += 1
     fileWriter.close();
 
 def main():
