cf8155d0c279c99ccf63e581c3acf5f1e41c5a8a
Adward-R <517908768@qq.com>
2015-01-11 13:36:36 +0800

    An Lei 's

---
+++ An/parseArticles.py
@@ -0,0 +1,41 @@
+import os
+
+fileName = open("namelist.txt", 'r')
+totalName = fileName.read()
+totalname = totalName.lower()
+for ii in range(54):
+	
+#	lastName = totalname[0 : totalname.find("\t")] 
+#	
+#	totalname = totalname[totalname.find("\t") + 1 : ]
+#	
+#	if ii == 53:
+#		firstName = totalname
+#	else:
+#		firstName = totalname[0 : totalname.find("\n")]
+#		
+#	currName = firstName + " " + lastName;
+#	totalname = totalname[totalname.find("\n") + 1 : ]
+#	
+#	print currName
+	currName = "international times";
+
+
+	for i in range(844):
+		
+		file = str(i + 1) + '.txt'
+		if not os.path.exists("trimmed/" + file):
+			continue
+		f = open("trimmed/" + file, 'r')
+		strInFile = f.read()
+		strinfile = strInFile.lower()
+		if (strinfile.find(currName) > 0):
+			currDir = "name/" + currName + "/";
+			if not os.path.exists(currDir):
+				os.mkdir(currDir)
+			fout = open(currDir + file, 'w')
+			fout.write(strInFile)
+			fout.close()
+		f.close()
+	
+
