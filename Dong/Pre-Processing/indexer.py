da04ed343ae2ad1944b12058af2726d0103cbb4e
Adward-R <517908768@qq.com>
2015-01-17 16:39:36 +0800

    deleting useless

--- Dong/Pre-Processing/indexer.py
+++ Dong/Pre-Processing/indexer.py
@@ -1,20 +1,20 @@
 #############################################################################
 #oh, no, I can not use chinese in this file ,sh*t
-#index V0.1, 
+#index V0.1,
 #read file start from 1.txt to *.txt
-## added (use replace instand) ###no re lib 
+## added (use replace instand) ###no re lib
 #--such as , . ; : ' " is in the index...   :(
 #differ lower like 'a' and upper 'A'
 ## added ###did not use stop words
 #did not use stemming
 ###########################################################################
 import sys, os
-stopwords = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the","happen","chance","chances");
+stopwords = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the");
 
 
 def read():
 	print '###############start###########################################################'
-	n=-1 
+	n= -1
 	index= []
 	path= sys.path[0]#.replace(':\\',':')
 	#sum=open(path+ "/sum.txt","w")
@@ -22,17 +22,20 @@ def read():
 		#open file ,if fail write back?
 		n= n+1
 		try:
-			f= open(path+ "/../../MC1 Data/MC1 Data/articles/%d.txt"%n,"r")
+			if (n==845):
+				break
+			else:
+				f= open(path+ "/docs/"+str(n)+".txt","r")
 		except IOError:
-			break
+			continue
 		print "analyzing the NO.%d doc"%n
 		#read file
 		i= 0
-		
+
 		line= f.readline()
 		for line in f:
 			i=i+1
-			
+
 			########################################################
 			templine= line.lower() #change to lower
 			templine= change(templine)
@@ -44,7 +47,7 @@ def read():
 			templine= templine.replace(':',' ')
 			templine= templine.replace(';',' ')
 			templine= templine.replace('=',' ')
-			templine= templine.replace('?',' ')	
+			templine= templine.replace('?',' ')
 			templine= templine.replace('~',' ')
 			templine= templine.replace('`',' ')
 			templine= templine.replace('_',' ')
@@ -80,7 +83,7 @@ def read():
 			templine= templine.replace('8',' ')
 			templine= templine.replace('9',' ')
 			editline= templine.split()
-			
+
 			#################################################
 			#start routine to read word
 			for word in editline:
@@ -88,7 +91,7 @@ def read():
 					#if in the stop words ,continue
 					stopwords.index(word)
 					continue
-				except ValueError:	
+				except ValueError:
 					try:
 						#if word is in index,add [doc,place]
 						k= index.index(word)
@@ -97,7 +100,7 @@ def read():
 						#else add word,add [[doc,place]]
 						index.append(word)
 						index.append([[n,i]])
-			
+
 		f.close()
 		#some information of the doc analyze
 		#sum.write("doc %d "%n)
@@ -106,24 +109,24 @@ def read():
 	print "start to write the index file"
 	#print the index in the result file
 	i= 0
-	
-	if not os.path.exists('wordIndex'):
-		os.mkdir('wordIndex')
+
+	if not os.path.exists('index'):
+		os.mkdir('index')
 	while 1:
 		try:
 			#read index such as ['a',[[1,2],[1,3]],.........
 			word= index[i]
-			newdoc= path + '/wordIndex/' + word
+			newdoc= path + '/index/' + word
 			print 'writing the index of '+ word
 			f= open(newdoc,'w')
 			post= index[i+1]  #post is [[1,2],[1,3],......
 			i= i+2
-			#f.write(word) 
-			#f.write('\n') 
+			#f.write(word)
+			#f.write('\n')
 			#read post such as [[1,2],[1,3],......
 			j= 0
 			t= 0
-			count= 0 
+			count= 0
 			query =''
 			while 1:
 				try:
@@ -144,7 +147,7 @@ def read():
 						count =1
 						t= a
 					j=j+1
-					
+
 				except IndexError:
 					#out of post, break
 					f.write("%d:"%count)
@@ -154,12 +157,12 @@ def read():
 					#f.write(word)
 					#f.write(' appear %d times' %j)
 					break
-				
+
 			f.close()
 		except IndexError:
 			#out of index, break
 			break
-	
+
 	#end.............
 	print 'the work finish'
 	print '###############finish###########################################################'
@@ -171,11 +174,11 @@ def change(str):
 			newstr+= ' '
 		else:
 			newstr+= char
-	return newstr	
-		
+	return newstr
+
 def main():
 
 	read()
-	
+
 if __name__ == '__main__':
 	main()
